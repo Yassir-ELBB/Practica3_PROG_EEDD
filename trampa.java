b764bfcb932d6a67d83cd7c67bacf02e25687885
Yassir-ELBB <yaselbadri@gmail.com>
2025-12-14 16:56:18 +0100

    Update trampa_Documentado.java

--- trampa.java
+++ trampa.java
@@ -2,10 +2,21 @@ package Ejercicio3;
 
 import java.util.Random;
 
+/**
+ * Clase trampa
+ * En esta clase se establecen las diferentes trampas, perjuicio y categoria de estas donde puede caer el jugador.
+ * @author yassirelbadribenjelloun
+ * @version 1
+ */
+
 public class trampa {
     private String categoria;
     private int perjuicio;
     private double fracaso;
+
+    /**
+     * Constructor por defecto que inicializa la categoria a vacia, perjuicio a 0 y fracaso a 0.
+     */
 
     public trampa(){
         categoria = "";
@@ -13,20 +24,37 @@ public class trampa {
         fracaso = 0;
     }
 
-
-
+    /**
+     * Getter que devuelve la catgoría de la trampa.
+     * @return categoria
+     */
 
     public String getCategoria(){
         return categoria;
     }
 
+    /**
+     * Getter que devuelve el perjuicio de la trampa.
+     * @return perjuicio
+     */
+
     public int getPerjuicio() {
         return perjuicio;
     }
 
+    /**
+     * Getter que devuleve la probabilidad de fracaso.
+     * @return fracaso
+     */
+
     public double getFracaso() {
         return fracaso;
     }
+
+    /**
+     * Seter que establece la categoría de la trampa entre las opciones de: Pinchos, Brea y Serpientes.
+     * @param Pcategoria
+     */
 
     public void setCategoria(String Pcategoria) {
         switch (Pcategoria){
@@ -46,12 +74,23 @@ public class trampa {
             categoria = Pcategoria;
     }
 
+    /**
+     * Seter que establece restricciones a la hora de indicar la cantidad de perjuicio
+     * Restricciones: El perjuicio no puede ser menor de 5 ni mayor a 20.
+     * @param Cperjuicio
+     */
+
     public void setPerjuicio(int Cperjuicio) {
         if(Cperjuicio < 5 || Cperjuicio > 20){
             System.err.println("Por favor elija un valor entre 5 y 20");
         } else System.out.println("Tu prejuicio es de: "+Cperjuicio);
         perjuicio = Cperjuicio;
     }
+
+    /**
+     * Método fracaso calcula la probabilidad de que la trampa fracase de forma aleatoria.
+     * @param Pfracaso
+     */
 
     public void Fracaso(double Pfracaso) {
         Random f = new Random();
@@ -62,6 +101,11 @@ public class trampa {
         fracaso = random;
 
     }
+
+    /**
+     * Método que activa la trampa de forma aleatoria y devolviendo si se ha caido en ella o no.
+     * @return caido
+     */
 
     public boolean activarTrampa(){
         double AT;
